a78ba7bdf2eed1638827f2933acf0333374a08ff
Michael Friis <friism@gmail.com>
2018-06-03 12:01:52 -0700

    remove akismet

--- wordpress/init.sh
+++ wordpress/init.sh
@@ -10,10 +10,10 @@ done
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
 
-wp plugin delete hello
-wp plugin install akismet wordpress-importer --activate
-wp plugin update akismet
-wp plugin activate akismet
+wp plugin delete hello akismet
+wp plugin install wordpress-importer --activate
+# wp plugin update akismet
+# wp plugin activate akismet
 
 wp option set permalink_structure "/%postname%/"
 
