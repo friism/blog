8ab5ea740b4de2e164d6a8f4ed83cdb6039bacaa
Michael Friis <friism@gmail.com>
2018-06-03 07:35:29 -0700

    activate importer plugin

--- wordpress/init.sh
+++ wordpress/init.sh
@@ -11,7 +11,7 @@ done
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
 
 wp plugin delete hello
-wp plugin install akismet wordpress-importer
+wp plugin install akismet wordpress-importer --activate
 wp plugin update akismet
 wp plugin activate akismet
 
