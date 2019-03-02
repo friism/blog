228d8b1c8d6342bf435810b5f162ae0357f86daf
Michael Friis <friism@gmail.com>
2019-03-02 11:28:28 -0800

    tweak init

--- wordpress/init.sh
+++ wordpress/init.sh
@@ -8,7 +8,9 @@ until wp db check; do
     sleep 1;
 done
 
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email \
+    --skip-themes
 
 wp plugin install akismet --activate
 wp plugin update akismet
