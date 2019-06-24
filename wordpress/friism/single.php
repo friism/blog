d41682912a6e807d475a39c2c47ef4cbc5f0f8ec
Michael Friis <friism@gmail.com>
2019-06-24 07:08:03 -0700

    simplify

--- wordpress/friism/single.php
+++ wordpress/friism/single.php
@@ -8,21 +8,18 @@
 
 get_header(); ?>
 
-	<article class="content-area col">
-		<?php
-		while ( have_posts() ) : the_post();
+	<?php
+	while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+		get_template_part( 'template-parts/content', get_post_format() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-		endwhile;
-		?>
-
-	</article>
+	endwhile;
+	?>
 
 <?php
 get_footer();
