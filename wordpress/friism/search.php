27a0ea3a52b99e29cddc4e5def841298f5c09e2f
Michael Friis <friism@gmail.com>
2019-11-17 10:34:21 -0800

    some bug

--- wordpress/friism/search.php
+++ wordpress/friism/search.php
@@ -6,32 +6,24 @@
  *
  */
 
-get_header(); ?>
+get_header();
 
-<?php if ( have_posts() ) : ?>
-
-	<h1><?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-
+if ( have_posts() ) {
+	?><h1>
+		<?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 	<?php
-	/* Start the Loop */
-	while ( have_posts() ) : the_post();
+	while ( have_posts() ) {
 
-		/**
-		 * Run the loop for the search to output the results.
-		 * If you want to overload this in a child theme then include a file
-		 * called content-search.php and that will be used instead.
-		 */
+		the_post();
 		get_template_part( 'template-parts/content', 'search' );
+	}
 
-	endwhile;
+	get_template_part( 'template-parts/content', 'list-nav' );
+}
+else {
 
 	get_template_part( 'template-parts/content', 'none' );
 
-else :
+}
 
-	get_template_part( 'template-parts/content', 'none' );
-
-endif; ?>
-
-<?php
 get_footer();
