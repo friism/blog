35d2178b9563472c23595eae07c850f1912f09dc
Michael Friis <friism@gmail.com>
2022-02-18 10:43:10 -0800

    render page content using normal content template

--- wordpress/friism/page.php
+++ wordpress/friism/page.php
@@ -15,6 +15,7 @@ get_header(); ?>
 	<main id="main" class="content-area col" role="main">
 		<?php
 		while ( have_posts() ) : the_post();
+			get_template_part( 'template-parts/content', get_post_format() );
 		endwhile;
 		?>
 	</main>
