e82148d5a57d83a65ebf4d942a223f379d7dce4b
Michael Friis <friism@gmail.com>
2024-04-20 06:36:24 -0700

    simplify post header

--- wordpress/friism/template-tags.php
+++ wordpress/friism/template-tags.php
@@ -22,25 +22,7 @@ function wp_bootstrap_starter_posted_on() {
 		esc_html( get_the_date() )
 	);
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'wp-bootstrap-starter' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'wp-bootstrap-starter' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span> | <span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
-    if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-        echo ' | <span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i> ';
-        /* translators: %s: post title */
-        comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'wp-bootstrap-starter' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-        echo '</span>';
-    }
-
+	echo '<span>' . $time_string . '</span>';
 }
 endif;
 
