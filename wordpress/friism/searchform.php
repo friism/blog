4cc60cd3ebf4d1a50e13192300fbdfa65f55a043
Michael Friis <friism@gmail.com>
2024-07-17 11:52:10 -0700

    remove classes

--- wordpress/friism/searchform.php
+++ wordpress/friism/searchform.php
@@ -1,7 +1,7 @@
-<form role="search" method="get" class="d-flex"
+<form role="search" method="get"
     action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="search" class="form-control me-2"
+    <input type="search"
         placeholder="Search" aria-label="Search" value="<?php echo esc_attr( get_search_query() ); ?>"
         name="s">
-    <button class="btn btn-outline-dark" type="submit">Search</button>
+    <button type="submit">Search</button>
 </form>
